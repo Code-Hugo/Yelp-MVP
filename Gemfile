9ced39ab40791e435320a2dfb1e18b8ea0189cba
Hugo Hodinka <hugo@travelperk.com>
2022-01-20 18:43:28 +0100

    Push 2nd excercise

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-
+gem 'faker'
 ruby '2.7.4'
-
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.4'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+0.times do
+  Restaurant.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.number(digits: 10),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
